7256b97a6e286a379a4f0cecd24674ee7657bcb2
Yimura <andreas.maerten@scarlet.be>
2022-05-29 13:11:44 +0200

    fix(start.sh): Metamod, SourceMod, startup, ...
    Added MetaMod and SourceMod
    Added steamcmd wrapper
    Fixed server startup warnings

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 APP_ID=740
-SERVER_DIR=/srv/
+SERVER_DIR=/srv
+
+# steamcmd wrapper
+function steamcmd() { bash $(which steamcmd.sh) "$@"; }
 
 function update_steamcmd()
 {
     echo "Updating steamcmd..."
-    steamcmd.sh +quit
+    steamcmd +quit
     echo "Finished updating steamcmd."
 }
 
@@ -15,7 +18,7 @@ function download_steam_app()
     install_dir=$2
 
     echo "Installing AppId:$app_id to $install_dir"
-    steamcmd.sh \
+    steamcmd \
         +force_install_dir "$install_dir" \
         +login anonymous \
         +app_update "$app_id" \
@@ -23,23 +26,60 @@ function download_steam_app()
     echo "Finished installing AppId:$app_id"
 }
 
-function start_server()
+function install_sourcemod()
 {
-    if [ "$EUID" == 0 ]
-    then
-        echo "Script running as root; starting server as unprivileged user."
+    server_directory=$1
+
+    METAMOD_VERSION=1.11
+    SOURCEMOD_VERSION=1.11
+
+    if [ ! -f "$server_directory/addons/metamod.vdf" ]; then
+        echo "Metamod isn't installed, installing..."
+        mkdir -p "$server_directory/addons/metamod"
+
+        LATEST_METAMOD=$(wget -qO- https://mms.alliedmods.net/mmsdrop/"$METAMOD_VERSION"/mmsource-latest-linux)
+        wget -qO- https://mms.alliedmods.net/mmsdrop/"$METAMOD_VERSION"/"$LATEST_METAMOD" | tar zxf - -C "$server_directory"
+
+        echo "Installed MetaMod"
     fi
 
+    if [ ! -f "$server_directory/addons/metamod/sourcemod.vdf" ]; then
+        echo "SourceMod isn't installed, installing..."
+        mkdir -p "$server_directory/addons/sourcemod"
+
+        LATEST_SOURCEMOD=$(wget -qO- https://sm.alliedmods.net/smdrop/"$SOURCEMOD_VERSION"/sourcemod-latest-linux)
+        wget -qO- https://sm.alliedmods.net/smdrop/"$SOURCEMOD_VERSION"/"$LATEST_SOURCEMOD" | tar zxf - -C "$server_directory"
+
+        echo "Installed SourceMod"
+    fi
+}
+
+function start_server()
+{
     server_directory=$1
     server_license=$2
+    workshop_api_key=$3
 
-    echo "Starting server with license '$server_license'"
+    echo "Starting server on 0.0.0.0:$SRV_PORT [tickrate:$SRV_TICKRATE] with xargs \"$SRV_ARGS\""
 
     cd $server_directory
-    ./srcds_run +sv_setsteamaccount $server_license -port 27015 -game csgo -console -usercon +game_mode 0 +mapgroup mg_active +map de_dust2
+
+    export LD_LIBRARY_PATH="$server_directory":"$server_directory/bin":{$LD_LIBRARY_PATH}
+    ./srcds_linux \
+        -authkey $workshop_api_key \
+        -tickrate $SRV_TICKRATE \
+        -port $SRV_PORT \
+        -game csgo \
+        -console \
+        -nobots \
+        +sv_setsteamaccount $server_license \
+        $SRV_ARGS
 }
+
+echo "Script starting as '$(whoami)'"
 
 update_steamcmd
 download_steam_app $APP_ID $SERVER_DIR
+install_sourcemod "$SERVER_DIR/csgo"
 
-start_server $SERVER_DIR $GAME_SERVER_LICENSE
+start_server $SERVER_DIR $GAME_SERVER_LICENSE $WORKSHOP_API_KEY
